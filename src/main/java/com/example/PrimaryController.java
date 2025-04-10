29c616af5882833544edda8eaf516396b30dcb3a
CabreraOladeinde_Tayn <cabrt926@hsd.k12.or.us>
2025-04-10 09:44:05 -0700

    added code from samples skeleton code

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,24 @@
-package com.example;
-
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    private TextField tfAddTask;
+
+    @FXML
+    private TextField tfTask1;
+
+    @FXML
+    private TextField tfTask2;
+
+    @FXML
+    private TextField tfTask3;
+
+    @FXML
+    private TextField tfTask4;
+
+    @FXML
+    private TextField tfTask5;
+
 }
