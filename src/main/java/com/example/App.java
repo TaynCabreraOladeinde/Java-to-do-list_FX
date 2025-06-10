7bad00354c4eebc9035f68402a7c0bb69669948f
CabreraOladeinde_Tayn <cabrt926@hsd.k12.or.us>
2025-06-10 08:39:47 -0700

    Updated the start method in App.java to load the correct FXML file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,11 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("mainscene"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
+    scene = new Scene(loadFXML("primary"), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+}
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
